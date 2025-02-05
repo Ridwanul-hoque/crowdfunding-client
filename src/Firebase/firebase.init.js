0d125b846a6822d1fc7e740e0f45945606cb037b
Ridwanul Hoque <rhr06junhoque@gmail.com>
2025-02-05 16:48:18 +0600

    firebase error fixed

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD45n8RmBRSv0koLwQ7oXhZJB3-7ppc_DQ",
-  authDomain: "crowdfunding-6ef9b.firebaseapp.com",
-  projectId: "crowdfunding-6ef9b",
-  storageBucket: "crowdfunding-6ef9b.firebasestorage.app",
-  messagingSenderId: "452727919810",
-  appId: "1:452727919810:web:2b11d956c1f98b4d31575b"
+  apiKey: import.meta.env.VITE_apiKey ,
+  authDomain: import.meta.env.VITE_authDomain ,
+  projectId: import.meta.env.VITE_projectId ,
+  storageBucket: import.meta.env.VITE_storageBucket ,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId ,
+  appId: import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
